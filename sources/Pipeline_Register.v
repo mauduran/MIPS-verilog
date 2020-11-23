488e1e06ecf00c03c56fb2d7069dfb97fe48ca76
Mauricio Duran <is716122@iteso.mx>
2020-11-22 19:52:26 -0600

    Created pipeline register, to be used in every pipeline stage. Work done by Mauricio & Ivan (pair programming)

---
+++ sources/Pipeline_Register.v
@@ -0,0 +1,36 @@
+/******************************************************************
+* Description
+*	This is a pipeline register, takes N bits as input and passes them to N bits
+*	Output when clock signal goes down. 
+* Version:
+*	1.0
+* Authors:
+*	Mauricio Duran
+*	Ivan Iturbe
+******************************************************************/
+
+module Pipeline_Register 
+#
+(
+	parameter N = 32,
+
+)
+(	
+	input clk,
+	input reset,
+	input [N-1:0] dataIn,
+	input [N-1:0] dataOut
+);
+
+
+
+//Se hace switch case para realizar operaciones segun sea necesario  
+   always @ (negedge clk)
+     begin
+		if(reset)
+			dataOut <= 0;
+		else
+			dataOut <= dataIn
+     end
+	  
+endmodule 
